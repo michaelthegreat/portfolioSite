c67fe97a98ba52cb79a92eea885ddb98d14accbd
Michael Quintero <michaelq.quintero@gmail.com>
2018-12-28 22:02:59 -0500

    remove old site

--- client/src/app/portfolio/portfolio-item.ts
+++
@@ -1,6 +0,0 @@
-export class PortfolioItem {
-    title: string;
-    description: string;
-    image:string;
-    url: string;
-  }
--- client/src/app/work-experience-item/work-experience-item.ts
+++
@@ -1,9 +0,0 @@
-export class WorkExperienceItem {
-    companyName: string;
-    position: string;
-    description: string;
-    image:string;
-    dateStarted : string;
-    dateEnded : string;
-    skills: string[];
- }
