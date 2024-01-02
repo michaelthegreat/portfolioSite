c9abee7975a5d9abe0a638932a02af4452e9dde9
Michael Quintero <michaelq.quintero@gmail.com>
2024-01-02 14:41:22 -0500

    Add a link to the gallery

--- src/app/app-layout/main-layout/header/header.component.ts
+++ src/app/app-layout/main-layout/header/header.component.ts
@@ -15,4 +15,9 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public navigateToSection(section: string) {
+        window.location.hash = '';
+        window.location.hash = section;
+  }
+
 }
